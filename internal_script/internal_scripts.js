58ac346aebe03d203da50730b013e6a1d4600fa3
sekostanski <sekostanski@gmail.com>
2018-11-14 11:27:28 +0100

    Add files

---
+++ internal_script/internal_scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.about-us__gallery-slider').slick({
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+  });
